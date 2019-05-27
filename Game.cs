d53332fb6b72dc29d33fad542168d333e5def368
Niels <n.koster01@student.alfa-college.nl>
2019-05-27 10:35:39 +0200

    Make player a child of game Room class is now child of player Player has health, and methods that damage, heal and check if the player is still alive

--- Game.cs
+++ Game.cs
@@ -1,47 +1,18 @@
 ﻿using System;
+using Zuul;
 
 namespace ZuulCS
 {
 	public class Game
 	{
 		private Parser parser;
-		private Room currentRoom;
+        public Player player;
 
-		public Game ()
-		{
-			createRooms();
-			parser = new Parser();
-		}
-
-		private void createRooms()
-		{
-			Room outside, theatre, pub, lab, office;
-
-			// create the rooms
-			outside = new Room("outside the main entrance of the university");
-			theatre = new Room("in a lecture theatre");
-			pub = new Room("in the campus pub");
-			lab = new Room("in a computing lab");
-			office = new Room("in the computing admin office");
-
-			// initialise room exits
-			outside.setExit("east", theatre);
-			outside.setExit("south", lab);
-			outside.setExit("west", pub);
-
-			theatre.setExit("west", outside);
-
-			pub.setExit("east", outside);
-
-			lab.setExit("north", outside);
-			lab.setExit("east", office);
-
-			office.setExit("west", lab);
-
-			currentRoom = outside;  // start game outside
-		}
-
-
+        public Game()
+        {
+            player = new Player();
+            parser = new Parser();
+        }
 		/**
 	     *  Main play routine.  Loops until end of play.
 	     */
@@ -69,7 +40,7 @@ namespace ZuulCS
 			Console.WriteLine("Zuul is a new, incredibly boring adventure game.");
 			Console.WriteLine("Type 'help' if you need help.");
 			Console.WriteLine();
-			Console.WriteLine(currentRoom.getLongDescription());
+			Console.WriteLine(player.currentRoom.getLongDescription());
 		}
 
 		/**
@@ -95,7 +66,7 @@ namespace ZuulCS
 					goRoom(command);
                     break;
                 case "look":
-                    Console.WriteLine(currentRoom.getLongDescription());
+                    Console.WriteLine(player.currentRoom.getLongDescription());
                     break;
                 case "quit":
 					wantToQuit = true;
@@ -136,13 +107,13 @@ namespace ZuulCS
 			string direction = command.getSecondWord();
 
 			// Try to leave current room.
-			Room nextRoom = currentRoom.getExit(direction);
+			Room nextRoom = player.currentRoom.getExit(direction);
 
 			if (nextRoom == null) {
 				Console.WriteLine("There is no door to "+direction+"!");
 			} else {
-				currentRoom = nextRoom;
-				Console.WriteLine(currentRoom.getLongDescription());
+                player.currentRoom = nextRoom;
+				Console.WriteLine(player.currentRoom.getLongDescription());
 			}
 		}
 
---
+++ Player.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using ZuulCS;
+
+namespace Zuul
+{
+    public class Player
+    {
+        public Room currentRoom;
+        public float health;
+
+        public Player()
+        {
+            createRooms();
+        }
+
+        private void createRooms()
+        {
+            Room outside, theatre, pub, lab, office;
+
+            // create the rooms
+            outside = new Room("outside the main entrance of the university");
+            theatre = new Room("in a lecture theatre");
+            pub = new Room("in the campus pub");
+            lab = new Room("in a computing lab");
+            office = new Room("in the computing admin office");
+
+            // initialise room exits
+            outside.setExit("east", theatre);
+            outside.setExit("south", lab);
+            outside.setExit("west", pub);
+
+            theatre.setExit("west", outside);
+
+            pub.setExit("east", outside);
+
+            lab.setExit("north", outside);
+            lab.setExit("east", office);
+
+            office.setExit("west", lab);
+
+            currentRoom = outside;  // start game outside
+        }
+        
+        private void damage(float amount)
+        {
+            health -= amount;
+        }
+
+        private void heal(float amount)
+        {
+            health += amount;
+        }
+
+        private bool isAlive()
+        {
+            if (health > 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
